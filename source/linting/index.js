70a092b21db2d954a960e959e5b18ebd2c06af2d
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-11 21:42:16 +0100

    Added Linting generator

---
+++ source/linting/index.js
@@ -0,0 +1,20 @@
+import { Base } from 'yeoman-generator'
+
+export default class LintingGenerator extends Base {
+
+  //----------------------------------------
+  // Writing
+  //----------------------------------------
+
+  writing() {
+    this.log('LintingGenerator.writing...')
+  }
+
+  //----------------------------------------
+  // Install
+  //----------------------------------------
+
+  install() {
+    this.log('LintingGenerator.install')
+  }
+}
