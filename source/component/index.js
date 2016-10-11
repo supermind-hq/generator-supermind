5a05361b7a1d58a0ec5d858d5bfa12aab1df9993
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-11 21:41:51 +0100

    Added React Component generator

---
+++ source/component/index.js
@@ -0,0 +1,12 @@
+import { Base } from 'yeoman-generator'
+
+export default class ComponentGenerator extends Base {
+
+  //----------------------------------------
+  // Writing
+  //----------------------------------------
+
+  writing() {
+    this.log('ComponentGenerator.writing')
+  }
+}
