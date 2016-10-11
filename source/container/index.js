8c8e519058498e550636a7b3496310b93086ec6a
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-11 21:42:05 +0100

    Added React Container generator

---
+++ source/container/index.js
@@ -0,0 +1,12 @@
+import { Base } from 'yeoman-generator'
+
+export default class ContainerGenerator extends Base {
+
+  //----------------------------------------
+  // Writing
+  //----------------------------------------
+
+  writing() {
+    this.log('ContainerGenerator.writing')
+  }
+}
