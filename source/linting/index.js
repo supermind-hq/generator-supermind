4844976b4717088dbaf4eb6aa5eadd0d01e64f17
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-12 19:17:58 +0100

    Updated linting generator

--- source/linting/index.js
+++ source/linting/index.js
@@ -3,11 +3,27 @@ import { Base } from 'yeoman-generator'
 export default class LintingGenerator extends Base {
 
   //----------------------------------------
+  // Prompt User
+  //----------------------------------------
+
+  prompting() {
+    this.log('LintingGenerator.prompting')
+    return this.prompt([{
+      type: 'confirm',
+      name: 'flow',
+      message: 'Use flow type checker:',
+      default: true
+    }]).then((answers) => {
+      this.config.set(answers)
+    })
+  }
+
+  //----------------------------------------
   // Writing
   //----------------------------------------
 
   writing() {
-    this.log('LintingGenerator.writing...')
+    this.log('LintingGenerator.writing')
   }
 
   //----------------------------------------
