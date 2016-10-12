b984150971ba7c1b031af064bfc6ddce6cc00232
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-12 19:18:21 +0100

    Added error handling to gulp tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,31 +3,44 @@
 import del from 'del'
 import gulp from 'gulp'
 import pump from 'pump'
-import gutil from 'gulp-util'
 import babel from 'gulp-babel'
 import filter from 'gulp-filter'
 import uglify from 'gulp-uglify'
+import plumber from 'gulp-plumber'
 import sourcemaps from 'gulp-sourcemaps'
+import { log, colors } from 'gulp-util'
 import { padStart, startCase } from 'lodash'
 
 //----------------------------------------
 // Constants
 //----------------------------------------
 
-const { colors } = gutil
 const sourceGlob = 'source/**/*'
 const generatorGlob = 'source/*/index.js'
 const eventColors = {
+  default: 'magenta',
   added: 'green',
   changed: 'blue',
-  deleted: 'red'
+  renamed: 'yellow',
+  deleted: 'red',
+  error: 'red'
 }
+
+//----------------------------------------
+// Helpers
+//----------------------------------------
+
+const replaceDirname = message =>
+  message.replace(`${__dirname}/`, '')
+
+const logError = ({ name, message }) =>
+  log(`${colors.red(name)} ${replaceDirname(message)}`)
 
 //----------------------------------------
 // Task: Clean
 //----------------------------------------
 
-gulp.task('clean', () => del('generators'))
+gulp.task('clean', () => del.sync('generators'))
 
 //----------------------------------------
 // Task: Compile
@@ -37,17 +50,15 @@ gulp.task('compile', [ 'clean' ], (callback) => {
   const generatorFilter = filter(generatorGlob, { restore: true })
   pump([
     gulp.src(sourceGlob),
+    plumber(logError),
     generatorFilter,
     sourcemaps.init(),
-    babel({
-      plugins: [ 'add-module-exports' ]
-    }),
-    uglify({
-    }),
+    babel({ plugins: [ 'add-module-exports' ] }),
+    uglify(),
     sourcemaps.write('.'),
     generatorFilter.restore,
     gulp.dest('generators')
-  ], callback)
+  ], () => callback())
 })
 
 //----------------------------------------
@@ -57,9 +68,9 @@ gulp.task('compile', [ 'clean' ], (callback) => {
 gulp.task('watch', [ 'compile' ], () => {
   const watcher = gulp.watch(sourceGlob, [ 'compile' ])
   watcher.on('change', ({ type, path }) => {
-    const color = eventColors[type]
+    const color = eventColors[type] || eventColors.default
     const event = padStart(startCase(type), 8)
     const file = path.replace(`${__dirname}/`, '')
-    gutil.log(`${colors[color](event)} '${colors.cyan(file)}'`)
+    log(`${colors[color](event)} '${colors.cyan(file)}'`)
   })
 })
