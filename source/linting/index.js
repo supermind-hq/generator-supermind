ebe66a90f374ff5224bc164aa332d370a6e985e1
Matthew Wagerfield <matthew@wagerfield.com>
2016-10-13 19:33:48 +0100

    Updated linting generator

--- source/linting/index.js
+++ source/linting/index.js
@@ -1,4 +1,7 @@
 import { Base } from 'yeoman-generator'
+import packageDir from 'pkg-dir'
+// import { compact, map, forEach } from 'lodash'
+// import { join } from 'path'
 
 export default class LintingGenerator extends Base {
 
@@ -7,7 +10,6 @@ export default class LintingGenerator extends Base {
   //----------------------------------------
 
   prompting() {
-    this.log('LintingGenerator.prompting')
     return this.prompt([{
       type: 'confirm',
       name: 'flow',
@@ -19,11 +21,22 @@ export default class LintingGenerator extends Base {
   }
 
   //----------------------------------------
-  // Writing
+  // Configure
   //----------------------------------------
 
-  writing() {
-    this.log('LintingGenerator.writing')
+  configuring() {
+    const rootDir = packageDir.sync(this.resolved)
+    const useFlow = this.config.get('flow')
+    this.log('LintingGenerator.configuring:', rootDir, useFlow)
+    // const files = compact(map([
+    //   '.editorconfig',
+    //   '.eslintrc',
+    //   '.jshintrc',
+    //   flow && '.flowconfig'
+    // ], file => join(rootDir, file)))
+    // forEach(files, (file) => {
+    //   this.fs.copy(file, this.destinationPath(file))
+    // })
   }
 
   //----------------------------------------
